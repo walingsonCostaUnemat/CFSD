3d15e22d09430be5a7ccacaf71e7250a90775b12
walingsonCostaUnemat <64569036+walingsonCostaUnemat@users.noreply.github.com>
2024-06-26 16:03:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
+import qrcode
+import vobject
+from PIL import Image
 from dashboard.distribuicao_cidades import distribuicao_cidades
 from dashboard.escolhas_por_pelotao import escolhas_por_pelotao
 from dashboard.selecionar_colunas import selecionar_colunas
@@ -7,6 +11,51 @@ from dashboard.interesses_cuiaba_varzea_grande import interesses_cuiaba_varzea_g
 from dashboard.selecionar_colunas_capital import selecionar_colunas_capital
 from dashboard.cuiaba import gerar_relatorio
 from dashboard.interior import gerar_relatorio_interior  # Importar a nova função
+
+def carregar_dados(file_path):
+    def formatar_telefone(numero):
+        """
+        Formata um número de telefone para o formato (99)99999-9999 ou (99)9999-9999.
+
+        Args:
+        numero (str): Número de telefone a ser formatado.
+
+        Returns:
+        str: Número de telefone formatado.
+        """
+        numero_str = ''.join(filter(str.isdigit, str(numero)))  # Remove caracteres não numéricos
+        if len(numero_str) == 11:
+            return f"({numero_str[:2]}){numero_str[2:7]}-{numero_str[7:]}"
+        elif len(numero_str) == 10:
+            return f"({numero_str[:2]}){numero_str[2:6]}-{numero_str[6:10]}"
+        else:
+            return numero  # Retorna o número original se não tiver o tamanho esperado
+
+    data = pd.read_csv(file_path)
+    
+    if 'Telefone DDD' in data.columns:
+        data['Telefone DDD'] = data['Telefone DDD'].apply(formatar_telefone)
+    
+    return data
+
+def criar_qrcode_vcard(nome, telefone, email, whatsapp_url):
+    vcard = vobject.vCard()
+    vcard.add('fn').value = nome
+    vcard.add('tel').value = telefone
+    vcard.add('email').value = email
+    vcard.add('url').value = whatsapp_url
+    vcard.add('note').value = 'Desenvolvedor'
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=3,  # Reduzindo o tamanho do QR code
+        border=2,
+    )
+    qr.add_data(vcard.serialize())
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    return img
 
 def pagina_principal():
     st.title("Dashboard de Preferências de Cidades")
@@ -29,35 +78,8 @@ def pagina_principal():
 
 # Carregar os dados
 file_path = 'escolha.csv'
-
-
-def carregar_dados(file_path):
-    def formatar_telefone(numero):
-        """
-        Formata um número de telefone para o formato (99)99999-9999 ou (99)9999-9999.
-
-        Args:
-        numero (str): Número de telefone a ser formatado.
-
-        Returns:
-        str: Número de telefone formatado.
-        """
-        numero_str = ''.join(filter(str.isdigit, str(numero)))  # Remove caracteres não numéricos
-        if len(numero_str) == 11:
-            return f"({numero_str[:2]}){numero_str[2:7]}-{numero_str[7:]}"
-        elif len(numero_str) == 10:
-            return f"({numero_str[:2]}){numero_str[2:6]}-{numero_str[6:]}"
-        else:
-            return numero  # Retorna o número original se não tiver o tamanho esperado
-
-    data = pd.read_csv(file_path)
-    
-    if 'Telefone DDD' in data.columns:
-        data['Telefone DDD'] = data['Telefone DDD'].apply(formatar_telefone)
-    
-    return data
-
 data = carregar_dados(file_path)
+
 # Ajustar o nome da coluna "Justificativa "
 if "Justificativa " in data.columns:
     data = data.rename(columns={"Justificativa ": "Justificativa"})
@@ -66,7 +88,7 @@ if "Justificativa " in data.columns:
 data['Cidades Limpo'] = data['Cidades'].apply(lambda nome: nome.split(" - ")[1] if len(nome.split(" - ")) > 1 else nome.split(" - ")[0])
 
 st.sidebar.title("Navegação")
-escolha_local = st.sidebar.radio("Selecione a Localidade", ["Capital", "Interior"])
+escolha_local = st.sidebar.radio("Selecione a Localidade", ["Capital", "Interior"], index=None)
 
 if escolha_local == "Interior":
     modulo = st.sidebar.selectbox("Selecione o Módulo", ["Relatório do Interior", "Escolhas de Cidade por Pelotão", "Selecionar Colunas e Aplicar Filtros", "Distribuição de Alunos por Cidades"])
@@ -86,3 +108,17 @@ elif escolha_local == "Capital":
         gerar_relatorio()
 else:
     pagina_principal()
+
+# Adicionar QR Code na barra lateral inferior
+st.sidebar.markdown("---")
+st.sidebar.markdown("### Qr Code para contato")
+
+whatsapp_url = "https://api.whatsapp.com/send/?phone=%2B5566996352623&text&type=phone_number&app_absent=0"
+img = criar_qrcode_vcard("Walingson Costa", "+5566996352623", "walingson.costa@unemat.br", whatsapp_url)
+buf = BytesIO()
+img.save(buf)
+buf.seek(0)
+st.sidebar.image(buf, caption="Aponte a câmera")
+if __name__ == "__main__":
+    if escolha_local is None:
+        pagina_principal()
